5e1649d78113e1b6c497d34f80c0242cffd4088e
Max Whiten <madmaxfz@gmail.com>
2023-06-10 02:54:02 -0500

    It basically works!!! There is a drift that the particles pick up in a certain direction. Calculate center of mass and then perhaps remove the average acceleration from the system

--- n_body.py
+++ n_body.py
@@ -6,20 +6,25 @@ from vispy.visuals.transforms import MatrixTransform
 from vispy.scene.cameras import FlyCamera
 
 
-class NewtonMatrix(Markers):
+class NewtonMatrix(SceneCanvas):
     """
 
     """
-    def __init__(self, num_bods=10, mass=None, pos_0=None, vel_0=None, *args, **kwargs):
+    def __init__(self, num_bods=100, mass=None, pos_0=None, vel_0=None, *args, **kwargs):
         """
 
         :type T0: np.float64
         :param num_bods:
         """
-        super(NewtonMatrix, self).__init__(*args, **kwargs)
+        super(NewtonMatrix, self).__init__(title="Figuring out Markers and SHit", keys='interactive',
+                                           fullscreen=True, size=(800, 600))
         self.unfreeze()
+        self.warp = .5
+        self.view = self.central_widget.add_view()
+        self.view.camera = "turntable"
         self.zero = np.zeros((1, 3), dtype=np.float64)
-        self.G = 6.67430e-11
+        G = 6.67430e-11
+        self.G = G * 500
         self.T0 = 0
         self.T_MAX = 1000
         self.N_BODS = num_bods
@@ -28,7 +33,7 @@ class NewtonMatrix(Markers):
         if mass is None:
             self.mass = np.ndarray((self.N_BODS, 1), dtype=np.float64)
             # put a randomized distribution of masses here
-            self.mass = np.random.normal(loc=1000, scale=100, size=self.N_BODS)
+            self.mass = np.random.normal(loc=2000, scale=400, size=self.N_BODS)
             # print("N_BODS =", self.N_BODS)
             # print("MASS =", self.mass)
         else:
@@ -39,7 +44,7 @@ class NewtonMatrix(Markers):
             norm_pos = np.zeros((1, self.N_BODS), dtype=type(np.array(3, dtype=np.float64)))
             # put a randomized distribution of positions here
             _th = np.linspace(0, 2 * np.pi, self.N_BODS)
-            mag_pos = np.random.normal(loc=1000, scale=100, size=self.N_BODS)
+            mag_pos = np.random.normal(loc=500, scale=100, size=self.N_BODS)
             # print("mag_pos =", mag_pos, len(mag_pos))
             norm_pos = np.array([np.cos(_th), np.sin(_th), np.sin(_th - _th)]).transpose()
             # print("norm_pos =", norm_pos, len(norm_pos))
@@ -70,14 +75,22 @@ class NewtonMatrix(Markers):
         #     self.SM[0, 1 + n] = self.vel_0[n]
         # print(type(self.SM[1, 0]), type(self.SM[0, 1]))
 
-        self.freeze()
+        self.particles = Markers(parent=self.view.scene)
+        self.view.add(self.particles)
+
         self.SM[0, 0] = self.T0
+        dat = []
         for n in range(0, self.N_BODS - 1):
             self.SM[1 + n, 0] = self.pos_0[n]
             self.SM[0, 1 + n] = self.vel_0[n]
-
+            dat.append(self.pos_0[n])
+        print(dat)
         self.set_rel_posvel()
         self.set_accel()
+        self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
+        self.show()
+        self._timer = app.Timer(interval='auto', connect=self.iterate, start=True, app=self.app)
+        self.freeze()
 
     # def set_posvel(self):
     #     self.SM[0, 0] = self.T0
@@ -114,13 +127,13 @@ class NewtonMatrix(Markers):
                     dist_sqr *= dist_sqr
                     # print(i, j, ":dist_sqr (i>j) = ", dist_sqr)
                     if dist_sqr != 0:
-                        accel += self.SM[i, j] * (-self.G * self.mass[j] / dist_sqr)
+                        accel += self.warp * self.SM[i, j] * (-self.G * self.mass[j] / dist_sqr)
                 elif i < j:
-                    dist_sqr = np.linalg.norm(-self.SM[i, j], axis=0)
+                    dist_sqr = np.linalg.norm(-self.SM[j, i], axis=0)
                     dist_sqr *= dist_sqr
                     # print(i, j, ":dist_sqr (i<j) = ", dist_sqr)
                     if dist_sqr != 0:
-                        accel += -self.SM[i, j] * (-self.G * self.mass[j] / dist_sqr)
+                        accel += -self.warp * self.SM[j, i] * (-self.G * self.mass[j] / dist_sqr)
                 elif i == j:
                     dist_sqr = 0.0
                     # print(i, j, ":dist_sqr (i=j) = ", dist_sqr)
@@ -128,9 +141,9 @@ class NewtonMatrix(Markers):
             self.SM[j, j] = accel
         # [print("accel[", n, "] =", self.SM[n, n]) for n in range(1, self.N_BODS)]
 
-    def iterate(self):
+    def iterate(self, event):
         """ Use acceleration values to update vel, then pos."""
-        self.M_hist.append(self.SM)
+        # self.M_hist.append(self.SM)
         if self.T0 < self.T_MAX:
             self.T0 += 1
         else:
@@ -141,8 +154,8 @@ class NewtonMatrix(Markers):
             self.SM[0, n + 1] += self.SM[n + 1, n + 1]
             self.SM[n + 1, 0] += self.SM[0, n + 1]
             dat.append(self.SM[n + 1, 0])
-        self.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
-        self.update()
+        self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
+        # self.update()
         self.set_rel_posvel()
         self.set_accel()
 
@@ -150,6 +163,7 @@ class NewtonMatrix(Markers):
         data = self.M_hist[self.T0]
         self.set_data(pos=data[1:, 0], size=3, edge_color="red")
         self.update()
+
 
 def axis_visual(scale=1.0, parent=None):
     """
@@ -188,20 +202,19 @@ def axis_visual(scale=1.0, parent=None):
 
 def main(viz=None):
     # print("MAIN")
-    can = SceneCanvas(title="Testing Transforms", size=(800, 600), keys="interactive", show=True)
-    view = can.central_widget.add_view()
-    view.camera = "arcball"
-
-    matrix = NewtonMatrix()
-    view.add(matrix)
-    timer = app.Timer(iterations=matrix.T_MAX, interval=1)
-    timer.connect(matrix.iterate())
-    timer.start(0)
+    can = NewtonMatrix()
+    # view.camera = "arcball"
+    #
+    # matrix = NewtonMatrix()
+    # view.add(matrix)
+    # timer = app.Timer(iterations=matrix.T_MAX, interval=1)
+    # timer.connect(matrix.iterate())
+    # timer.start(0)
     # view.camera.set_range()
     # timer = app.Timer(connect=matrix.on_timer, iterations=matrix.T_MAX, start = True)
 
     can.show()
-    app.run()
+    can.app.run()
 
 
 # def iter_matrix():
