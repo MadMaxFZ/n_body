8f5102f3689cb217572888e68587dc06026a3894
Max S. Whitten <madmaxfz@gmail.com>
2023-06-10 17:59:18 -0500

    Now we get plenty of motion... but it's some sort of rounding error that send the entire system away from the origin... Try to subtract averages, with limited results.

--- n_body.py
+++ n_body.py
@@ -19,7 +19,7 @@ class NewtonMatrix(SceneCanvas):
         super(NewtonMatrix, self).__init__(title="Figuring out Markers and SHit", keys='interactive',
                                            fullscreen=True, size=(800, 600))
         self.unfreeze()
-        self.warp = .5
+        self.warp = 1000
         self.view = self.central_widget.add_view()
         self.view.camera = "turntable"
         self.zero = np.zeros((1, 3), dtype=np.float64)
@@ -29,11 +29,15 @@ class NewtonMatrix(SceneCanvas):
         self.T_MAX = 1000
         self.N_BODS = num_bods
         self.M_hist = []
+        self.avg_pos = self.zero.copy()
+        self.avg_vel = self.zero.copy()
+        self.avg_acc = self.zero.copy()
 
         if mass is None:
             self.mass = np.ndarray((self.N_BODS, 1), dtype=np.float64)
             # put a randomized distribution of masses here
-            self.mass = np.random.normal(loc=2000, scale=400, size=self.N_BODS)
+            self.mass = np.random.normal(loc=3000, scale=500, size=self.N_BODS)
+            self.mass[0] *= 1000
             # print("N_BODS =", self.N_BODS)
             # print("MASS =", self.mass)
         else:
@@ -44,7 +48,7 @@ class NewtonMatrix(SceneCanvas):
             norm_pos = np.zeros((1, self.N_BODS), dtype=type(np.array(3, dtype=np.float64)))
             # put a randomized distribution of positions here
             _th = np.linspace(0, 2 * np.pi, self.N_BODS)
-            mag_pos = np.random.normal(loc=500, scale=100, size=self.N_BODS)
+            mag_pos = np.random.normal(loc=300, scale=50, size=self.N_BODS)
             # print("mag_pos =", mag_pos, len(mag_pos))
             norm_pos = np.array([np.cos(_th), np.sin(_th), np.sin(_th - _th)]).transpose()
             # print("norm_pos =", norm_pos, len(norm_pos))
@@ -87,6 +91,7 @@ class NewtonMatrix(SceneCanvas):
         print(dat)
         self.set_rel_posvel()
         self.set_accel()
+        self.get_averages()
         self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
         self.show()
         self._timer = app.Timer(interval='auto', connect=self.iterate, start=True, app=self.app)
@@ -155,14 +160,28 @@ class NewtonMatrix(SceneCanvas):
             self.SM[n + 1, 0] += self.SM[0, n + 1]
             dat.append(self.SM[n + 1, 0])
         self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
+        trx = MatrixTransform()
+        trx.translate(-self.avg_pos)
+        self.particles.transform = trx
         # self.update()
         self.set_rel_posvel()
         self.set_accel()
 
-    def on_timer(self, event):
-        data = self.M_hist[self.T0]
-        self.set_data(pos=data[1:, 0], size=3, edge_color="red")
-        self.update()
+    def get_averages(self):
+        self.avg_pos = self.zero.copy()
+        self.avg_vel = self.zero.copy()
+        self.avg_acc = self.zero.copy()
+        for n in range(1, self.N_BODS + 1):
+            self.avg_pos += self.SM[n, 0]
+            self.avg_vel += self.SM[0, n]
+            self.avg_acc += self.SM[n, n]
+        self.avg_pos /= self.N_BODS
+        self.avg_vel /= self.N_BODS
+        self.avg_acc /= self.N_BODS
+        print("\n>>>>", self.T0, "<<<<")
+        print("AVG_POS =", self.avg_pos)
+        print("AVG_VEL =", self.avg_vel)
+        print("AVG_ACC =", self.avg_acc)
 
 
 def axis_visual(scale=1.0, parent=None):
@@ -212,7 +231,9 @@ def main(viz=None):
     # timer.start(0)
     # view.camera.set_range()
     # timer = app.Timer(connect=matrix.on_timer, iterations=matrix.T_MAX, start = True)
-
+    frame = axis_visual(scale=1000, parent=can.view.scene)
+    can.view.add(frame)
+    # can.particles.parent=frame
     can.show()
     can.app.run()
 
