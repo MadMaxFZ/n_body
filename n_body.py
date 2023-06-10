6b46260433253cfb08f6965a0fa5f42bebb42d4e
Max S. Whitten <madmaxfz@gmail.com>
2023-06-10 16:01:02 -0500

    Now we get plenty of motion... but it's some sort of rounding error that send the entire system away from the origin... Try to subtract averages, with limited results.

--- n_body.py
+++ n_body.py
@@ -16,6 +16,7 @@ class NewtonMatrix(SceneCanvas):
         :type T0: np.float64
         :param num_bods:
         """
+        global norm_pos, mag_pos
         super(NewtonMatrix, self).__init__(title="Figuring out Markers and SHit", keys='interactive',
                                            fullscreen=True, size=(800, 600))
         self.unfreeze()
@@ -29,11 +30,15 @@ class NewtonMatrix(SceneCanvas):
         self.T_MAX = 1000
         self.N_BODS = num_bods
         self.M_hist = []
+        self.avg_pos = self.zero.copy()
+        self.avg_vel = self.zero.copy()
+        self.avg_acc = self.zero.copy()
 
         if mass is None:
             self.mass = np.ndarray((self.N_BODS, 1), dtype=np.float64)
             # put a randomized distribution of masses here
             self.mass = np.random.normal(loc=2000, scale=400, size=self.N_BODS)
+            self.mass[0] *= 1000
             # print("N_BODS =", self.N_BODS)
             # print("MASS =", self.mass)
         else:
@@ -50,6 +55,7 @@ class NewtonMatrix(SceneCanvas):
             # print("norm_pos =", norm_pos, len(norm_pos))
             self.pos_0 = np.array([mag_pos[n] * norm_pos[n] for n in range(0, self.N_BODS)])
             # print("pos_0 =", self.pos_0, len(self.pos_0))
+            self.pos_0[0] = self.zero.copy()
         else:
             self.pos_0 = pos_0
 
@@ -59,6 +65,7 @@ class NewtonMatrix(SceneCanvas):
             dv = np.array(np.sin(np.random.normal(loc=0, scale=15 * np.pi / 180, size=self.N_BODS)))
             # print("dv =", dv)
             self.vel_0 = [norm_pos[n] * dv[n] + np.cross([0, 0, 1], norm_pos[n]) / np.sqrt(mag_pos[n]) for n in range(0, self.N_BODS)]
+            # self.vel_0[0] = self.zero.copy()
         else:
             self.vel_0 = vel_0
 
@@ -84,19 +91,16 @@ class NewtonMatrix(SceneCanvas):
             self.SM[1 + n, 0] = self.pos_0[n]
             self.SM[0, 1 + n] = self.vel_0[n]
             dat.append(self.pos_0[n])
-        print(dat)
+        # print(dat)
         self.set_rel_posvel()
         self.set_accel()
+        self.get_averages()
         self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
         self.show()
         self._timer = app.Timer(interval='auto', connect=self.iterate, start=True, app=self.app)
         self.freeze()
 
-    # def set_posvel(self):
-    #     self.SM[0, 0] = self.T0
-    #     for n in range(0, self.N_BODS - 1):
-    #         self.SM[1 + n, 0] = self.pos_0[n]
-    #         self.SM[0, 1 + n] = self.vel_0[n]
+# =========================================================================================================
 
     def set_rel_posvel(self):
         """
@@ -129,7 +133,7 @@ class NewtonMatrix(SceneCanvas):
                     if dist_sqr != 0:
                         accel += self.warp * self.SM[i, j] * (-self.G * self.mass[j] / dist_sqr)
                 elif i < j:
-                    dist_sqr = np.linalg.norm(-self.SM[j, i], axis=0)
+                    dist_sqr = np.linalg.norm(self.SM[j, i], axis=0)
                     dist_sqr *= dist_sqr
                     # print(i, j, ":dist_sqr (i<j) = ", dist_sqr)
                     if dist_sqr != 0:
@@ -138,7 +142,10 @@ class NewtonMatrix(SceneCanvas):
                     dist_sqr = 0.0
                     # print(i, j, ":dist_sqr (i=j) = ", dist_sqr)
 
-            self.SM[j, j] = accel
+            if j != 1:
+                self.SM[j, j] = accel - self.avg_acc
+            else:
+                self.SM[j, j] = self.zero.copy()
         # [print("accel[", n, "] =", self.SM[n, n]) for n in range(1, self.N_BODS)]
 
     def iterate(self, event):
@@ -148,21 +155,38 @@ class NewtonMatrix(SceneCanvas):
             self.T0 += 1
         else:
             pass
-        dat = []
-        for n in range(0, self.N_BODS):
+        dat = [self.SM[1, 0]]
+        for n in range(1, self.N_BODS):
             self.SM[0, 0] = self.T0
             self.SM[0, n + 1] += self.SM[n + 1, n + 1]
             self.SM[n + 1, 0] += self.SM[0, n + 1]
+            # self.SM[n + 1, 0] -= self.avg_pos
             dat.append(self.SM[n + 1, 0])
+
         self.particles.set_data(pos=np.array(dat), size=4, edge_color="red", edge_width=1)
-        # self.update()
         self.set_rel_posvel()
         self.set_accel()
 
-    def on_timer(self, event):
-        data = self.M_hist[self.T0]
-        self.set_data(pos=data[1:, 0], size=3, edge_color="red")
-        self.update()
+    def get_averages(self):
+        self.avg_pos = self.zero.copy()
+        self.avg_vel = self.zero.copy()
+        self.avg_acc = self.zero.copy()
+        for n in range(1, self.N_BODS + 1):
+            self.avg_pos += self.SM[n, 0]
+            self.avg_vel += self.SM[0, n]
+            self.avg_acc += self.SM[n, n]
+        self.avg_pos /= self.N_BODS
+        self.avg_vel /= self.N_BODS
+        self.avg_acc /= self.N_BODS
+        print("\n>>>>", self.T0, "<<<<")
+        print("AVG_POS =", self.avg_pos)
+        print("AVG_VEL =", self.avg_vel)
+        print("AVG_ACC =", self.avg_acc)
+
+    # def on_timer(self, event):
+    #     data = self.M_hist[self.T0]
+    #     self.set_data(pos=data[1:, 0], size=3, edge_color="red")
+    #     self.update()
 
 
 def axis_visual(scale=1.0, parent=None):
@@ -191,61 +215,13 @@ def axis_visual(scale=1.0, parent=None):
 
     return axis
 
-# def update():
-#     global matrix, t, data
-#     data = matrix.M_hist[t]
-#     matrix.set_data(pos=np.array([data[n, 0] for n in range(1, matrix.N_BODS + 1)]), size=4, edge_color="red")
-#     # matrix.update()
-#     t += 1
-#     t = t % 999
 
-
-def main(viz=None):
-    # print("MAIN")
+def main():
     can = NewtonMatrix()
-    # view.camera = "arcball"
-    #
-    # matrix = NewtonMatrix()
-    # view.add(matrix)
-    # timer = app.Timer(iterations=matrix.T_MAX, interval=1)
-    # timer.connect(matrix.iterate())
-    # timer.start(0)
-    # view.camera.set_range()
-    # timer = app.Timer(connect=matrix.on_timer, iterations=matrix.T_MAX, start = True)
-
+    axis = axis_visual(scale=1000)
+    can.view.add(axis)
     can.show()
     can.app.run()
-
-
-# def iter_matrix():
-#     # N = 10
-#     # MAX_T = 10
-#     # T = 0
-#     # mass = [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., ]
-#     # theta = np.linspace(0, 359, N)
-#     # radius = np.random.normal(loc=1000, scale=100, size=N)
-#     # pos_0 = np.zeros((N, 3), dtype=np.float64)
-#     # vel_0 = np.zeros((N, 3), dtype=np.float64)
-#     # vel0_dir = np.zeros((N, 3), dtype=np.float64)
-#
-#     # for n in range(0, N - 1):
-#     #     r = radius[n]
-#     #     ct = np.cos(theta[n])
-#     #     st = np.sin(theta[n])
-#     #     print(r, ct, st)
-#     #     pos_0[n] = [r * ct, r * st, 0]
-#     #     vel0_dir[n] = np.cross(pos_0[n], [0.0, 0.0, 1.0]) / np.linalg.norm(pos_0[n])
-#     #     vel_0[n] = vel0_dir[n] * (1 / np.sqrt(np.linalg.norm(pos_0[n])))
-#
-#
-#
-#     while NM.T0 < NM.T_MAX:
-#         NM.iterate()
-#
-#     return NM
-#
-#     print(np.array(NM.M_hist).shape)
-
 
 if __name__ == '__main__':
     main()
