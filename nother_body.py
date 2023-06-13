19f2a30678e424e4cf9d03300a67dc8c5223d29a
Max S. Whitten <madmaxfz@gmail.com>
2023-06-12 21:21:24 -0500

    refactoring the code to take advantage of numpy vectorization. Hopefully it will run faster. Consider whether to calculate everything first, then replay the data with a visual OR just update a visual as we go, OR BOTH!!!

--- nother_body.py
+++ nother_body.py
@@ -37,11 +37,13 @@ class NewtonMatrix(SceneCanvas):
         self.warp = 0.01                            # 'time' elapsed per frame
         self.T_0 = t_start                          # 'time' at start of simulation
         self.T_1 = t_end                            # 'time' at end of simulation
-        self.T_n = np.linspace(start=self.T_0,
-                               stop=self.T_1,
-                               num=self.N_frames)
+        self.T_n = np.linspace(start=self.ticks,
+                               stop=self.N_frames,
+                               num=self.N_frames,
+                               )
         if has_star:                                # if central mass desired,
             num_bods += 1                           #   add another spot for it
+
         self.N_bods = num_bods                      # number of particles to create
         self.zero = np.zeros(3, dtype=np.float64)   # default zero vector
         self.cm_pos = self.zero.copy()              # center of mass position
@@ -59,19 +61,41 @@ class NewtonMatrix(SceneCanvas):
         self.matrix = np.zeros((self.N_frames,
                                 self.N_bods + 1,
                                 self.N_bods + 1,
-                                3), dtypre=np.float64)
+                                3), dtype=np.float64)
 
-        self.frame_t = self.matrix.[:, 0, 0]
+        self.frame_t = self.matrix[:, 0, 0]
         self.frame_t = self.T_n
         self.pos = self.matrix[:, 1:, 0]
         self.vel = self.matrix[:, 0, 1:]
         self.rel_posvel = self.matrix[:, 1:, 1:]
 
         self.view = self.central_widget.add_view()
-        self.view.camera = "arcball"
+        self.particles = Markers(parent=self.view.scene)
+        self.view.add(self.particles)
+        self.set_positions()
+
         self.freeze()
 
-    # ==============================================================================
+    # =========================================================================================
+
+    def set_positions(self):
+        """
+
+        :return:
+        :rtype:
+        """
+        norm_pos = np.zeros((self.N_bods, 3), dtype=np.float64)
+        # put a randomized distribution of positions here
+        thetas = np.linspace(0, 2 * np.pi, self.N_bods)
+        _ph = 0
+        mag_pos = np.random.normal(loc=200, scale=50, size=self.N_bods)
+        # print("mag_pos =", mag_pos, len(mag_pos))
+        norm_pos = np.array(
+            [(np.cos(t), np.sin(t), np.sin(_ph)) for t in thetas])  # , dtype=type(np.array(3, dtype=np.float64)))
+        mag_pos[0] = 0
+        # print("norm_pos =", norm_pos, len(norm_pos))
+        self.pos = [mag_pos[n] * norm_pos[n] for n in range(0, self.N_bods)]
+
 
     def set_relposvel(self):
         """
@@ -95,9 +119,10 @@ class NewtonMatrix(SceneCanvas):
             for i in range(0, self.N_bods):
 
                 if j != i:
-                    accel += -self.G * self.mass[i] / np.power(self.rel_posvel[self.T_n, 2])
+                    accel += -self.G * self.mass[i] / np.power(self.rel_posvel[self.T_n, i, j, 2])
 
             self.matrix[self.ticks, j, j] = accel
+            self.accel[self.ticks, j] = accel
 
 
     def update_posvel(self):
@@ -106,4 +131,90 @@ class NewtonMatrix(SceneCanvas):
         :return:
         :rtype:
         """
-        self.vel[]
+        self.ticks += 1
+        self.vel[self.ticks] += self.accel[self.ticks - 1] * self.warp
+        self.pos[self.ticks] += self.vel[self.ticks] * self.warp
+
+def axis_visual(scale=1.0, parent=None):
+    """
+    Returns a :class:`vispy.scene.visuals.XYZAxis` class instance using given
+    scale.
+
+    Parameters
+    ----------
+    scale : numeric, optional
+        Axis visual scale.
+    parent : Node, optional
+        Parent of the axis visual in the `SceneGraph`.
+
+    Returns
+    -------
+    XYZAxis
+        Axis visual.
+    """
+
+    axis = XYZAxis(parent=parent)
+
+    transform = MatrixTransform()
+    transform.scale((scale, scale, scale))
+    axis.transform = transform
+
+    return axis
+
+
+def main(viz=None):
+    # print("MAIN")
+
+    can = NewtonMatrix()
+    # view.camera = "arcball"
+    #
+    # matrix = NewtonMatrix()
+    # view.add(matrix)
+    # timer = app.Timer(iterations=matrix.T_MAX, interval=1)
+    # timer.connect(matrix.iterate())
+    # timer.start(0)
+    # view.camera.set_range()
+    # timer = app.Timer(connect=matrix.on_timer, iterations=matrix.T_MAX, start = True)
+    frame = axis_visual(scale=1000000, parent=can.view.scene)
+    trx = MatrixTransform()
+    trx.rotate(-90, [1, 0, 0])
+    trx.scale((1, 1))
+    frame.transform = trx
+    can.view.add(frame)
+    # can.particles.parent=frame
+    can.show()
+    can.app.run()
+
+
+# def iter_matrix():
+#     # N = 10
+#     # MAX_T = 10
+#     # T = 0
+#     # mass = [1., 1., 1., 1., 1., 1., 1., 1., 1., 1., ]
+#     # theta = np.linspace(0, 359, N)
+#     # radius = np.random.normal(loc=1000, scale=100, size=N)
+#     # pos_0 = np.zeros((N, 3), dtype=np.float64)
+#     # vel_0 = np.zeros((N, 3), dtype=np.float64)
+#     # vel0_dir = np.zeros((N, 3), dtype=np.float64)
+#
+#     # for n in range(0, N - 1):
+#     #     r = radius[n]
+#     #     ct = np.cos(theta[n])
+#     #     st = np.sin(theta[n])
+#     #     print(r, ct, st)
+#     #     pos_0[n] = [r * ct, r * st, 0]
+#     #     vel0_dir[n] = np.cross(pos_0[n], [0.0, 0.0, 1.0]) / np.linalg.norm(pos_0[n])
+#     #     vel_0[n] = vel0_dir[n] * (1 / np.sqrt(np.linalg.norm(pos_0[n])))
+#
+#
+#
+#     while NM.T0 < NM.T_MAX:
+#         NM.iterate()
+#
+#     return NM
+#
+#     print(np.array(NM.M_hist).shape)
+
+
+if __name__ == '__main__':
+    main()
